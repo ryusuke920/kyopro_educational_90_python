13c98082566d3d3c54f49af33e2034beed07996e
ryusuke920 <ryusuke920@AtCoder-Library.com>
2021-04-16 01:07:10 +0900

    mkdir Similar problem

---
+++ solve_python/008.py
@@ -0,0 +1,20 @@
+s = input()
+mod = 10 ** 9 + 7
+a = at = atc = atco = atcod = atcode = atcoder = 0
+for i in s:
+    if i == "a":
+        a += 1
+    elif i == "t":
+        at += a
+    elif i == "c":
+        atc += at
+    elif i == "o":
+        atco += atc
+    elif i == "d":
+        atcod += atco
+    elif i == "e":
+        atcode += atcod
+    elif i == "r":
+        atcoder += atcode
+    print(i,a,at,atc,atco,atcod,atcode,atcoder)
+print(atcoder)
