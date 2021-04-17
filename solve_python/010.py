abd21727513285d67fafe4ac72e9b3754e62d20b
ryusuke920 <ryusuke920@AtCoder-Library.com>
2021-04-17 12:18:42 +0900

    modified 010

--- solve_python/010.py
+++ solve_python/010.py
@@ -2,12 +2,16 @@ n = int(input())
 c, p = [0] * n, [0] * n
 for i in range(n):
     c[i], p[i] = map(int,input().split())
-one = [0] * n
-two = [0] * n
+
+one = [0] * n # １組の累積和
+two = [0] * n # ２組の累積和
+# 初項を決める
 if c[0] == 1:
     one[0] = p[0]
 else:
     two[0] = p[0]
+
+# 累積和を取っていく
 for i in range(n - 1):
     if c[i + 1] == 1:
         one[i + 1] += one[i] + p[i + 1]
