1fcc4fd9cb4608bcc07849bf0b69dde6d291226d
ryusuke920 <ryusuke920@AtCoder-Library.com>
2021-04-30 17:22:16 +0900

    solve 012

--- solve_python/012.py
+++ solve_python/012.py
@@ -1,5 +1,3 @@
-# まだ解けていません
-from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -10,10 +8,10 @@ def find(x):
         par[x] = find(par[x])
         return par[x]
 
-def same(x,y):
+def same(x, y):
     return find(x) == find(y)
 
-def unite(x,y):
+def unite(x, y):
     x = find(x)
     y = find(y)
     if x == y:
@@ -23,23 +21,21 @@ def unite(x,y):
     par[x] = y
 
 h, w = map(int,input().split())
-par = [i for i in range(h * w)]
 q = int(input())
-grid = [False] * (h * w) # 赤色 -> True, 白色 -> False
+
+par = [i for i in range(h * w + 2)]
+grid = [[False] * (w + 2) for _ in range(h + 2)]
+d = [[-1, 0], [1, 0], [0, -1], [0, 1]] # ４方向に対する変化量
+
 for i in range(q):
     a = list(map(int,input().split()))
     if a[0] == 1:
-        grid[w * (a[1] - 1) + (a[2] - 1)] = True
-    elif a[0] == 2:
-        unite(w * (a[1] - 1) + (a[2] - 1), w * (a[3] - 1) + (a[4] - 1))
-        if same(w * (a[1] - 1) + (a[2] - 1), w * (a[3] - 1) + (a[4] - 1)) and grid[w * (a[1] - 1) + (a[2] - 1)] and grid[w * (a[3] - 1) + (a[4] - 1)]:
+        grid[a[1]][a[2]] = True
+        for dy, dx in d:
+            if grid[a[1] + dy][a[2] + dx] == True: # 周囲４方向が塗られていれば、連結させる
+                unite((a[1] + dy - 1) * w + (a[2] + dx), (a[1] - 1) * w + a[2])
+    elif a[0] == 2: # 連結している & 2つのマスが赤色になっていればYes
+        if same((a[1] - 1) * w + a[2], (a[3] - 1) * w + a[4]) and grid[a[1]][a[2]] and grid[a[3]][a[4]]:
             print("Yes")
         else:
             print("No")
-
-print(par)
-ans = [0] * (h * w)
-for i in range(h * w):
-    ans[find(i)] += 1
-print(ans)
-print(*grid)
