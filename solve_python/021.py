f92d178f7263f63e9670c709de3b7e7384436872
ryusuke920 <ryusuke920@AtCoder-Library.com>
2021-04-30 19:59:38 +0900

    solve 021

--- solve_python/021.py
+++ solve_python/021.py
@@ -1,60 +1,80 @@
 import sys
 input = sys.stdin.readline
 
-def SCC(to, ot):
-    # まだ通ってない -> -1, 行きに通った -> 0, 帰りに通った -> 1, dfsも済んだ -> 2
-    check = [-1] * n
-    q = []
-    for i in range(n):
-        if check[i] != -1: continue
-        stack = [i]
+class SCC:
+    def __init__(self, n):
+        self.n = n
+        self.graph = [[] for _ in range(n)]
+        self.rev_graph = [[] for _ in range(n)]
+        self.labels = [-1] * n
+        self.lb_cnt = 0
+
+    def add_edge(self, v, nxt_v):
+        self.graph[v].append(nxt_v)
+        self.rev_graph[nxt_v].append(v)
+
+    def build(self):
+        self.post_order = []
+        self.used = [False] * self.n
+        for v in range(self.n):
+            if not self.used[v]:
+                self._dfs(v)
+        for v in reversed(self.post_order):
+            if self.labels[v] == -1:
+                self._rev_dfs(v)
+                self.lb_cnt += 1
+
+    def _dfs(self, v):
+        stack = [v, 0]
+        while stack:
+            v, idx = stack[-2:]
+            if not idx and self.used[v]:
+                stack.pop()
+                stack.pop()
+            else:
+                self.used[v] = True
+                if idx < len(self.graph[v]):
+                    stack[-1] += 1
+                    stack.append(self.graph[v][idx])
+                    stack.append(0)
+                else:
+                    stack.pop()
+                    self.post_order.append(stack.pop())
+
+    def _rev_dfs(self, v):
+        stack = [v]
+        self.labels[v] = self.lb_cnt
         while stack:
             v = stack.pop()
-            if check[v] == -1:
-                check[v] = 0
-                for j in to[v]:
-                    if check[j] != -1: continue
-                    stack.append(j)
-            else:
-                stack.pop()
-                if check[v] == 0:
-                    check[v] = 1
-                    q.append(v)
-        
-    # 逆辺でdfs
-    num = []
-    while q:
-        v = q.pop()
-        if check[v] != 1: continue
-        check[v] = 2
-        now = [v]
-        i = 0
-        while i < len(now):
-            v = now[i]
-            for j in ot[v]:
-                if check[j] == 2: continue
-                check[j] = 2
-                now.append(j)
-            i += 1
-        num.append(now)
-    
-    return num
+            for nxt_v in self.rev_graph[v]:
+                if self.labels[nxt_v] != -1:
+                    continue
+                stack.append(nxt_v)
+                self.labels[nxt_v] = self.lb_cnt
 
+    def construct(self):
+        self.dag = [[] for i in range(self.lb_cnt)]
+        self.groups = [[] for i in range(self.lb_cnt)]
+        for v, lb in enumerate(self.labels):
+            for nxt_v in self.graph[v]:
+                nxt_lb = self.labels[nxt_v]
+                if lb == nxt_lb:
+                    continue
+                self.dag[lb].append(nxt_lb)
+            self.groups[lb].append(v)
+        return self.dag, self.groups
 
-n, m = map(int,input().split())
-a, b = [0] * m, [0] * m
+n, m = map(int, input().split())
+graph = [list(map(int,input().split())) for _ in range(m)]
+scc = SCC(n)
 
-to = [[] for _ in range(n)]
-ot = [[] for _ in range(n)]
-
-for i in range(m):
-    a[i], b[i] = map(int,input().split())
-    to[a[i] - 1].append(b[i] - 1)
-    ot[b[i] - 1].append(a[i] - 1)
+for u, v in graph:
+    scc.add_edge(u - 1, v - 1)
+scc.build()
+_,elems = scc.construct()
 
 ans = 0
-for i in SCC(to, ot):
-    cnt = len(i)
-    ans += cnt * (cnt - 1) // 2
+for i in range(len(elems)):
+    ans += len(elems[i]) * (len(elems[i]) - 1) // 2
 
 print(ans)
