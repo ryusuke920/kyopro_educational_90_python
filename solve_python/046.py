8355df05b50dfbce909e3f1312e2699952173a2c
ryusuke920 <ryusuke920@AtCoder-Library.com>
2021-05-21 10:57:16 +0900

    solve 046

---
+++ solve_python/046.py
@@ -0,0 +1,22 @@
+n = int(input())
+a = sorted(list(map(int,input().split())))
+b = sorted(list(map(int,input().split())))
+c = sorted(list(map(int,input().split())))
+
+cnt_a , cnt_b, cnt_c = [0] * 46, [0] * 46, [0] * 46
+
+# 46で割った余りがいくつあるかを記録していく
+for i in range(n):
+    cnt_a[a[i] % 46] += 1
+    cnt_b[b[i] % 46] += 1
+    cnt_c[c[i] % 46] += 1
+
+ans = 0
+for i in range(46):
+    for j in range(46):
+        for k in range(46):
+            # ３つの余りが46の倍数であるとき、答えを足す
+            if (i + j + k) % 46 == 0:
+                ans += cnt_a[i] * cnt_b[j] * cnt_c[k]
+
+print(ans)
