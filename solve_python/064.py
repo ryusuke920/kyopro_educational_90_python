11560e02373706db7f4bb1564256ed3a786a6224
ryusuke920 <ryusuke920@AtCoder-Library.com>
2021-06-18 10:29:28 +0900

    solve064

--- solve_python/064.py
+++ solve_python/064.py
@@ -1,10 +1,19 @@
 n, q = map(int,input().split())
 a = list(map(int,input().split()))
-b = []
-for i in range(n - 1):
-    b.append(a[i + 1] - a[i])
+
+b = [a[i + 1] - a[i] for i in range(n - 1)] # 階差数列を記録する配列
+now = sum(abs(i) for i in b) # 現時点での不便さ
 
 for i in range(q):
     l, r, v = map(int,input().split())
-
-print(b)
+    if l > 1:
+        l -= 2
+        now -= abs(b[l])
+        b[l] += v
+        now += abs(b[l])
+    if r < n:
+        r -= 1
+        now -= abs(b[r])
+        b[r] -= v
+        now += abs(b[r])
+    print(now)
