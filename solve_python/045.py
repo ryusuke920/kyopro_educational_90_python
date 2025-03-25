bd3f04c90aa2102ed9c209ac05195ed5a6c1eef9
ryusuke920 <ryusuke_920@icloud.com>
2025-03-25 13:53:05 +0900

    テンプレートを作成

---
+++ solve_python/045.py
@@ -0,0 +1,10 @@
+def calc(x1: int, y1: int, x2: int, y2: int) -> int:
+    return (x1 - x2)**2 + (y1 - y2)**2
+
+
+N, K = map(int, input().split())
+X, Y = [0]*N, [0]*N
+for i in range(N):
+    X[i], Y[i] = map(int, input().split())
+
+# dp[i][j] := 点(Xj, Yj) を i 個のグループに分けた時の最小コスト
